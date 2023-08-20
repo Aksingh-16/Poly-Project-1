ebf379438a7bb5cd18c7dbd7d3a0e4a969bf5d69
Aksingh-16 <125183985+Aksingh-16@users.noreply.github.com>
2023-08-20 16:44:17 +0530

    mint.js

--- Scripts/mint.js
+++ Scripts/mint.js
@@ -1,34 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/Project1.json");
+const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/Akash.json");
 
-const tokenAddress = "0x0A450a84b1eCC3a21b6E9f9B177E942252c4DC7D";
+const tokenAddress = "0x73900Dd062bf28586719b18eB81B6849D83fcd9F";
 const tokenABI = tokenContractJSON.abi;
-const walletAddress = "0x56A75D486a273C891c223Cb7BD305651549B5DA9"; 
+const walletAddress = "0xDdFeF4e1FEa36677e90ab6e54547BD4e628271d4"; 
 
+const lst = ["ipfs://QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH","ipfs://QmWpxcMjXADF9mEAkvpPSSAqGb4MEW4LtP7G97jXtjJJ8P", "ipfs://Qmd729Pw6GDhzyarSCgxq47kkZ8zVaYmsrgYBdzn5duLKU" ,"ipfs://QmVEz7TD1To7fYJrbZJX7xqBA9oRoxd4N5GvFDujSSbrW6", "ipfs://QmPz9iEtuGJn845ditXzsXvnrxEt9s4g7LSszzpC9v6cAP"]
 
-const lst = ["ipfs://QmbzLiawt5LjQhcsdgrM6YYigxGyRuqVnPTK77xAWPXjZb","ipfs://QmZXeNuzLmdKjgsYa4jdt7XMegeEkwyhTY9HidAY9qdFdp", "ipfs://QmdzKXnLJ3VG6wS2GLKgbKmf1oxoEhj4VCBenB3NPJLogM" ,"ipfs://QmSqcFJL2tAB9Jju5ooVZibUpsfMAhAGh5PbAYiUCnDF8D", "ipfs://QmYM54bk33qowude1TbuE6jH4uUVAsTJtHoS4MjBTZGtkY"]
 async function main() {
-
     const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
     
-    for(let i=0;i<=lst.length;i++){
-      const tx = await token.mint(walletAddress,i+5,lst[i]);
+    for(let i = 0; i < lst.length; i++){
+      const tx = await token.mint(walletAddress, i+6, lst[i]);
       await tx.wait();
     }
-  
+    
+    const balance = await token.balanceOf(walletAddress);
+    console.log(`You now have: ${balance.toString()} tokens`);
+}
 
-    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
-  }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
-  });
+});
